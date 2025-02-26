f084c2b391fdc47478a14ff33b19cce686c76f66
F Cary Snyder <fcarysnyder@gmail.com>
2025-02-25 17:25:23 -0800

    ready to deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.fcarysnyder.com',
+  site: 'https://fcarysnyder.com',
+
 });
